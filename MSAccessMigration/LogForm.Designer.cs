744c7e80a762888de98d2e4cc6ddd140af3376a9
sarvesh.upadhyay <sarvesh.upadhyay@SARVESH-TI10353>
2017-07-27 18:28:39 +0530

    Log improved

--- MSAccessMigration/LogForm.Designer.cs
+++ MSAccessMigration/LogForm.Designer.cs
@@ -33,16 +33,25 @@
             this.label1 = new System.Windows.Forms.Label();
             this.txtLogFile = new System.Windows.Forms.TextBox();
             this.btnLogFile = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.dtfilter = new System.Windows.Forms.DateTimePicker();
+            this.btnFilter = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.dtEndTime = new System.Windows.Forms.DateTimePicker();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // txtLog
             // 
             this.txtLog.BackColor = System.Drawing.SystemColors.HighlightText;
             this.txtLog.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtLog.Location = new System.Drawing.Point(2, 99);
+            this.txtLog.Location = new System.Drawing.Point(2, 223);
             this.txtLog.Name = "txtLog";
             this.txtLog.ReadOnly = true;
-            this.txtLog.Size = new System.Drawing.Size(1693, 718);
+            this.txtLog.Size = new System.Drawing.Size(1693, 594);
             this.txtLog.TabIndex = 0;
             this.txtLog.Text = "";
             // 
@@ -77,11 +86,101 @@
             this.btnLogFile.UseVisualStyleBackColor = true;
             this.btnLogFile.Click += new System.EventHandler(this.btnLogFile_Click);
             // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(98, 91);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(377, 26);
+            this.textBox1.TabIndex = 4;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(497, 87);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(128, 39);
+            this.btnSearch.TabIndex = 5;
+            this.btnSearch.Text = "&Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // dtfilter
+            // 
+            this.dtfilter.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.dtfilter.Location = new System.Drawing.Point(116, 151);
+            this.dtfilter.Name = "dtfilter";
+            this.dtfilter.Size = new System.Drawing.Size(92, 26);
+            this.dtfilter.TabIndex = 6;
+            this.dtfilter.ValueChanged += new System.EventHandler(this.dtfilter_ValueChanged);
+            // 
+            // btnFilter
+            // 
+            this.btnFilter.Location = new System.Drawing.Point(477, 151);
+            this.btnFilter.Name = "btnFilter";
+            this.btnFilter.Size = new System.Drawing.Size(132, 34);
+            this.btnFilter.TabIndex = 7;
+            this.btnFilter.Text = "&Filter";
+            this.btnFilter.UseVisualStyleBackColor = true;
+            this.btnFilter.Click += new System.EventHandler(this.btnFilter_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(649, 151);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(160, 34);
+            this.btnClear.TabIndex = 8;
+            this.btnClear.Text = "&Clear Filter";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // dtEndTime
+            // 
+            this.dtEndTime.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.dtEndTime.Location = new System.Drawing.Point(356, 151);
+            this.dtEndTime.Name = "dtEndTime";
+            this.dtEndTime.Size = new System.Drawing.Size(93, 26);
+            this.dtEndTime.TabIndex = 9;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(17, 156);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 20);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Start Time";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(249, 156);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(76, 20);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "End Time";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(-2, 94);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(94, 20);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Search Text";
+            // 
             // LogForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1735, 829);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dtEndTime);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnFilter);
+            this.Controls.Add(this.dtfilter);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.btnLogFile);
             this.Controls.Add(this.txtLogFile);
             this.Controls.Add(this.label1);
@@ -89,6 +188,7 @@
             this.Name = "LogForm";
             this.Text = "LogForm";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.LogForm_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -101,5 +201,14 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox txtLogFile;
         private System.Windows.Forms.Button btnLogFile;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.DateTimePicker dtfilter;
+        private System.Windows.Forms.Button btnFilter;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.DateTimePicker dtEndTime;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
     }
 }
--- MSAccessMigration/LogForm.cs
+++ MSAccessMigration/LogForm.cs
@@ -29,5 +29,88 @@ namespace MSAccessMigration
                 txtLog.Text =await AppLogManager.ReadLog(txtLogFile.Text.Trim());
             }
         }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            Cursor.Current = Cursors.WaitCursor;
+            if (!string.IsNullOrEmpty(textBox1.Text.Trim()))
+                {
+                txtLog.SelectionStart = 0;
+                txtLog.SelectionLength = txtLog.TextLength;
+                txtLog.SelectionBackColor = Color.White;
+                int j = 0, i = 0;
+                var searchText = textBox1.Text.Trim();
+
+                do
+                {
+                    j = txtLog.Text.IndexOf(searchText, i, StringComparison.InvariantCultureIgnoreCase);
+                    if (j >= 0)
+                    {
+                        txtLog.SelectionStart = j;
+                        txtLog.SelectionLength = searchText.Length;
+                        txtLog.SelectionBackColor = Color.Yellow;
+                        // txtLog.Refresh();
+                        i = j + 1;
+
+                    }
+
+                } while (j >= 0);
+            }
+            else { MessageBox.Show("Please enter text to search"); }
+            Cursor.Current = Cursors.Arrow;
+        }
+
+        private void btnFilter_Click(object sender, EventArgs e)
+        {
+            Cursor.Current = Cursors.WaitCursor;
+
+            var starttime = dtfilter.Text.Trim();
+            var endtime = dtEndTime.Text.Trim();
+            var lines = txtLog.Lines.ToList();
+            int startIndex = 0,endIndex=0;
+
+            for(int index=0; index<lines.Count;index++)
+            {
+                if (lines[index].Contains(starttime)) {
+                    startIndex = index;
+                    break;
+                }
+            }
+
+            for (int index = lines.Count-1; index > 0; index--)
+            {
+                if (lines[index].Contains(endtime))
+                {
+                    endIndex = index;
+                    break;
+                }
+            }
+
+
+
+
+            var linefiltered = lines.Skip(startIndex).Take(endIndex - startIndex);
+            txtLog.Clear();
+            txtLog.Lines = linefiltered.ToArray();
+            Cursor.Current = Cursors.Arrow;
+        }
+
+        private void LogForm_Load(object sender, EventArgs e)
+        {
+            dtfilter.CustomFormat = "HH:mm";
+            dtfilter.Format = DateTimePickerFormat.Custom;
+            dtEndTime.CustomFormat = "HH:mm";
+            dtEndTime.Format = DateTimePickerFormat.Custom;
+        }
+
+        private async void btnClear_Click(object sender, EventArgs e)
+        {
+            txtLog.Text = await AppLogManager.ReadLog(txtLogFile.Text.Trim());
+        }
+
+        private void dtfilter_ValueChanged(object sender, EventArgs e)
+        {
+            dtEndTime.Text = dtfilter.Text;
+        }
     }
 }
